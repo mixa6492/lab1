fac6072da75e5adaa3df316b3f108bfe1c59eedd
Stefan <niketic95@gmail.com>
2017-03-14 01:34:43 +0100

    Added clck 27-50 MHz started clck_counter

--- projects/1_basic/src/rtl/clk_counter.vhd
+++ projects/1_basic/src/rtl/clk_counter.vhd
@@ -29,13 +29,45 @@ END clk_counter;
 
 ARCHITECTURE rtl OF clk_counter IS
 
-SIGNAL   counter_r : STD_LOGIC_VECTOR(25 DOWNTO 0);
+component reg is
+	generic(
+		WIDTH    : positive := 26;
+		RST_INIT : integer := 0
+	);
+	port(
+		i_clk  : in  std_logic;
+		in_rst : in  std_logic;
+		i_d    : in  std_logic_vector(WIDTH-1 downto 0);
+		o_q    : out std_logic_vector(WIDTH-1 downto 0)
+	);
+end component reg;
+
+
+
+
+signal   cnt : STD_LOGIC_VECTOR(25 DOWNTO 0);
+signal	cnt1: STD_LOGIC_VECTOR(25 DOWNTO 0);
+signal	cnt2 : STD_LOGIC_VECTOR(25 DOWNTO 0);
+signal	cnt3 : STD_LOGIC_VECTOR(25 DOWNTO 0);
+signal	cnt4 : STD_LOGIC_VECTOR(25 DOWNTO 0);
+signal	one_sec: std_logic;
 
 BEGIN
 
 -- DODATI:
 -- brojac koji kada izbroji dovoljan broj taktova generise SIGNAL one_sec_o koji
 -- predstavlja jednu proteklu sekundu, brojac se nulira nakon toga
+
+
+-- Registar
+r1: reg PORT MAP (
+                i_clk  => clk_i,
+                in_rst => not(rst_i),
+                i_d    => cnt4,
+                o_q   => cnt
+                );
+--Sabirac
+cnt1<=cnt+1;
 
 
 END rtl;
