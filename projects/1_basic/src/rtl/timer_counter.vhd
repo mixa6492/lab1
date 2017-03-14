4c90e77aa6e5907d500d17ba1f0cfafc08b73704
Stefan <niketic95@gmail.com>
2017-03-14 01:44:59 +0100

    Completed timer_counter

--- projects/1_basic/src/rtl/timer_counter.vhd
+++ projects/1_basic/src/rtl/timer_counter.vhd
@@ -25,13 +25,43 @@ ENTITY timer_counter IS PORT (
 END timer_counter;
 
 ARCHITECTURE rtl OF timer_counter IS
+
+component reg is
+	generic(
+		WIDTH    : positive := 1;
+		RST_INIT : integer := 0
+	);
+	port(
+		i_clk  : in  std_logic;
+		in_rst : in  std_logic;
+		i_d    : in  std_logic_vector(WIDTH-1 downto 0);
+		o_q    : out std_logic_vector(WIDTH-1 downto 0)
+	);
+end component reg;
+
 SIGNAL counter_value_r : STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL next_value : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
 BEGIN
 
 -- DODATI :
 -- brojac koji na osnovu izbrojanih sekundi pravi izlaz na LE diode
 
+reg1 : reg
+generic map(
+	WIDTH => 8
+)
+port map(
+	i_clk => clk_i,
+	in_rst => rst_i,
+	i_d => next_value,
+	o_q => counter_value_r
+);
 
+next_value <= counter_value_r + 1 when cnt_en_i = '1' and one_sec_i = '1' else
+				counter_value_r;
+		
+-- DODELA IZLAZA --		
+led_o <=  counter_value_r;
 
 END rtl;
